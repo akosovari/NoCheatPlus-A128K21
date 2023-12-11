b397a5d2b63c10c4b702a77ef815f351e26be2c0
TheForgTN <akosvr@gmail.com>
2023-12-11 07:44:30 +0100

    Fix fly / speed exploit.

--- NCPCore/src/main/java/fr/neatmonster/nocheatplus/PlayerSpeed.java
+++ NCPCore/src/main/java/fr/neatmonster/nocheatplus/PlayerSpeed.java
@@ -5,10 +5,7 @@ import fr.neatmonster.nocheatplus.checks.moving.MovingConfig;
 import fr.neatmonster.nocheatplus.checks.moving.MovingData;
 import fr.neatmonster.nocheatplus.players.DataManager;
 import fr.neatmonster.nocheatplus.players.IPlayerData;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.Server;
-import org.bukkit.World;
+import org.bukkit.*;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -31,19 +28,6 @@ public class PlayerSpeed implements Listener {
 
 
         player.getScheduler().runAtFixedRate(getServer().getPluginManager().getPlugin("NoCheatPlus"), scheduledTask -> {
-
-
-
-            // PlayerSpeed
-            // Maybe this helps with people teleporting through Multiverse portals having problems?
-            final IPlayerData pData = DataManager.getPlayerData(player);
-            final MovingData data_mov = pData.getGenericInstance(MovingData.class);
-            Material m = player.getLocation().getBlock().getType();
-            if(m == Material.NETHER_PORTAL || m== Material.END_PORTAL) {
-                data_mov.resetSetBack();
-            }
-
-
             if(last_location.containsKey(player) && player.getLocation().getWorld() == last_location.get(player).getWorld()){
                 if(player.getLocation() != last_location.get(player)){
                     float delta_time = (float) (System.currentTimeMillis() - last_check.get(player))/1000;
@@ -56,5 +40,19 @@ public class PlayerSpeed implements Listener {
         }, null, 1L, 10L);
 
     }
+    @EventHandler
+    public void onJoinSpeed_Exp(PlayerJoinEvent e){
+        Player player = e.getPlayer();
+        player.getScheduler().runAtFixedRate(getServer().getPluginManager().getPlugin("NoCheatPlus"), scheduledTask -> {
+            // PlayerSpeed / Fly exploit
+            //
+            final IPlayerData pData = DataManager.getPlayerData(player);
+            final MovingData data_mov = pData.getGenericInstance(MovingData.class);
+            Material m = player.getLocation().getBlock().getType();
+            if(m == Material.NETHER_PORTAL || m== Material.END_PORTAL) {
+                data_mov.resetSetBack();
+            }
+        }, null, 1L, 1L);
 
+    }
 }
